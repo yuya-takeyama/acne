9771ffd325403ba8cc55cf6cb4175b0bfa86062f
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-09-02 12:12:36 +0900

    Add comments

--- src/Acne/Util.php
+++ src/Acne/Util.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * This file is part of Acne.
+ *
+ * (c) Yuya Takeyama
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Util class for Acne.
+ *
+ * @author Yuya Takeyama
+ */
 class Acne_Util
 {
     public static function isServiceProvider($value)
